541d37fa1a5a2cbd952ffb3e3d3a1d76fc697f8a
Bhavana G <bhavanag.17cs@saividya.ac.in>
2020-07-13 22:32:19 +0530

    Add files via upload

---
+++ EventManagementSystems/database/events.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2019 at 06:39 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `csefest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(100) NOT NULL,
+  `event_title` text NOT NULL,
+  `event_price` int(20) NOT NULL,
+  `participents` int(100) NOT NULL,
+  `img_link` text NOT NULL,
+  `type_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `event_title`, `event_price`, `participents`, `img_link`, `type_id`) VALUES
+(1, 'pubg', 50, 4, 'cs01.jpg', 2),
+(2, 'tech quiz', 50, 2, 'cs02.jpg', 1),
+(3, 'counter strike', 50, 1, 'cs03.jpg', 2),
+(4, 'pair programming', 50, 2, 'cs01.jpg', 1),
+(5, 'seminar', 50, 1, 'cs02.jpg', 3),
+(6, 'multiple choice', 50, 1, 'cs01.jpg', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_type`
+--
+
+CREATE TABLE `event_type` (
+  `type_id` int(10) NOT NULL,
+  `type_title` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event_type`
+--
+
+INSERT INTO `event_type` (`type_id`, `type_title`) VALUES
+(1, 'Technical Events'),
+(2, 'Gaming Events'),
+(3, 'On Stage Events'),
+(4, 'Off Stage Events'),
+(5, 'new event Events');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participent`
+--
+
+CREATE TABLE `participent` (
+  `user_id` int(10) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `college` varchar(300) NOT NULL,
+  `branch` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `participent`
+--
+
+INSERT INTO `participent` (`user_id`, `fullname`, `email`, `password`, `mobile`, `college`, `branch`) VALUES
+(1, 'puneeth reddy', 'puneethreddy951@gmail.com', '25f9e794323b453885f5181f1b624d0b', '9535688928', 'RRCE', 'CSE');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `event_type`
+--
+ALTER TABLE `event_type`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `participent`
+--
+ALTER TABLE `participent`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `event_type`
+--
+ALTER TABLE `event_type`
+  MODIFY `type_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `participent`
+--
+ALTER TABLE `participent`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
