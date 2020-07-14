3a0ddf2e955f8034d46e994329418698b2d1a8c1
Bhavana G <bhavanag.17cs@saividya.ac.in>
2020-07-14 14:03:21 +0530

    Add files via upload

---
+++ EventManagementSystems/utils/adminHeader.php
@@ -0,0 +1,47 @@
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Sanchalana 2k20</title>
+    <style>
+.bgImage 
+{
+    background-image: url(images/cs03.jpg);
+    background-size: cover;
+    background-position: center center;
+    height: 600px;
+    margin-bottom: 29px;
+}
+</style>
+</head>
+
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+
+
+<header class="bgImage" > 
+    <nav class="navbar" >
+        <div class="container">
+            <div class="navbar-header"><!--website name/title-->
+            
+                  <h1 style="color:#ffffff">  Sanchalana 2k20</h1>
+                
+            </div>
+            <ul class="nav navbar-nav navbar-right"><!--navigation-->
+            <li><a href = "adminPage.php"><strong>Home</strong></a></li>
+            <li><a href = "Stu_details.php"><strong>Student Details</strong></a></li>
+                    <li><a href = "Stu_cordinator.php"><strong>Student Co-ordinator</strong></a></li>
+                    <li><a href = "Staff_cordinator.php"><strong>Staff-Co-ordinator</strong></a></li>
+                    <li class="btnlogout"><a class = "btn btn-default navbar-btn" href = "index.php">Logout <span class = "glyphicon glyphicon-log-out"></span></a></li>            
+        
+            </ul>
+        </div><!--container div-->
+    </nav>
+    <div class = "col-md-12">
+        <div class = "container">
+            <div class = "jumbotron"><!--jumbotron-->
+                <h1><strong>Welcome to Admin Page</strong></h1><!--jumbotron heading-->
+                <div class="browse d-md-flex col-md-14" >
+                <div class="row">
+                  
+            </div>
+        </div>
+    </div>
+</header>
---
+++ EventManagementSystems/utils/styles.php
@@ -0,0 +1,2 @@
+<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+<link rel = "stylesheet" type = "text/css" href = "css/style.css">
